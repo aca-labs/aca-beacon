d927fbde8c72b6a4b3e9cb5512cbee7ea373a483
Stephen von Takach <steve@acaprojects.com>
2019-03-14 14:42:21 +1100

    init commit

---
+++ ACABeacon/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ACABeacon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mac_addresses.Text = String.Join(",", MACAddress.get_mac_addresses());
+            username.Text = MACAddress.get_domain_name() + "\\" + MACAddress.get_username();
+        }
+    }
+}
---
+++ ACABeacon/mac_address.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace ACABeacon
+{
+    class MACAddress
+    {
+        public static string[] get_mac_addresses() {
+            var query = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n =>
+                    n.OperationalStatus == OperationalStatus.Up && // only grabbing what's online
+                    n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .Select(_ => new
+                {
+                    PhysicalAddress = _.GetPhysicalAddress(),
+                    IPProperties = _.GetIPProperties(),
+                });
+
+            var macs = query
+                .Where(q => q.IPProperties.UnicastAddresses.Any(ua => true)).ToArray();
+
+            string[] human_readable = new string[macs.Length];
+            var index = 0;
+
+            foreach (var mac in macs) {
+                human_readable[index] = String.Join("-", mac.PhysicalAddress.GetAddressBytes().Select(b => b.ToString("X2")));
+                index += 1;
+            }
+
+            return human_readable;
+        }
+
+        public static string get_username() {
+            return Environment.UserName;
+        }
+
+        public static string get_domain_name() {
+            return Environment.UserDomainName;
+        }
+    }
+}
